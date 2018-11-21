cbf29fb29a61f1387d4cc0f7d88070c013a2aa50
Wesley Angus <wesman93@gmail.com>
2018-11-21 15:11:48 -0500

    Removed unneeded files, added dynamic NavMesh baking using NavMeshSurface

---
+++ Assets/Scripts/NavMeshBaker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    public NavMeshSurface[] surfaces;
+
+    // Use this for initialization
+    void Start()
+    {
+        //bakeTime = Time.time + bakeDelay;
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            surfaces[i].BuildNavMesh();
+        }
+    }
+}
